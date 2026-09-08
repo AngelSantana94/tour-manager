75b3077d341b0c579bf25767a9c6f646b76e9eea
AngelSantana94 <ignaciosantana373@gmail.com>
2026-09-08 22:33:16 +0200

    añadiendo funcion voucher test12

--- api/vouchers/[id].ts
+++ api/vouchers/[id].ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../../src/types/database.types";
+import type { Database } from "../../src/types/database.types.js";
 
 const BUCKET = "vouchers";
 
